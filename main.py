d75a600b223c5996c24dd7b994bfe214c4d67fec
Ruan Chengzhi <161990346+RCZ-code@users.noreply.github.com>
2024-10-16 09:13:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import Add_evlr
 import Analyze_evlr
+import Read_evlr
 
 LsPath = "./LAS/"
 PtPath = "./PoTree/"
 OtPath = "./Output/"
 
 Add_evlr.Add_evlr_to_pt(LsPath, PtPath, OtPath)
+Read_evlr.read_evlr()
 pt = Analyze_evlr.Potree('./Output/output_with_evlr.las')
 position = pt.get_data_by_LOD(['position'],0)
 print(position)
