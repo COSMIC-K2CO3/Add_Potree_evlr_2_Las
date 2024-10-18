9ed1ca5251ba6d8d3ddb17b90e820b7014ce9da9
Ruan Chengzhi <161990346+RCZ-code@users.noreply.github.com>
2024-10-18 15:44:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ OtPath = "./Output/"
 
 Add_evlr.Add_evlr_to_pt(LsPath, PtPath, OtPath)
 Read_evlr.read_evlr()
+
 pt = Analyze_evlr.Potree('./Output/output_with_evlr.las')
 position = pt.get_data_by_LOD(['position'],0)
 print(position)
+# rgb = pt.get_data_by_LOD(['rgb'],0)
+# print(rgb)
